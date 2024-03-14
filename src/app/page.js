0238d5e15913589c080c4de59eb055329c65d6b8
Vishal <Vishalarya2468@gmail.com>
2024-03-14 16:11:03 -0700

    Initial Commit

---
+++ src/app/page.js
@@ -0,0 +1,25 @@
+"use client"
+import Theme from "@/components/Theme/page";
+import ChatBot from "@/components/chatbot/page";
+
+export default function Home() {
+  const {theme,themes,handleTheme} = Theme();
+
+  return (
+    <main
+      className={`flex min-h-screen items-center justify-around bg-primary theme-${theme} `}
+    >
+ 
+    </main>
+  );
+}
+
+{/* <div className="mt-16 text-white">Home</div>
+<div className="theme-selection flex  ">
+  {themes.map((t) => (
+    <div key={t.id} onClick={()=>handleTheme(t)} className="text-tBase mx-16 border-2 border-borderclr px-8 py-2  rounded-md cursor-pointer">
+      {t}
+    </div>
+  ))}
+</div>
+<ChatBot/> */}
---
+++ src/app/self-help/page.jsx
@@ -0,0 +1,7 @@
+const SelfHelp = () => {
+  return (
+    <div className='mt-16 text-white'>page</div>
+  )
+}
+
+export default SelfHelp;
